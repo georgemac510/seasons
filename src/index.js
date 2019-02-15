6b5852f8148dbcde6907557dca7a0eac5985d49d
georgemac510 <georgemac510@gmail.com>
2019-02-14 21:04:00 -0500

    Add existing files

--- src/index.js
+++ src/index.js
@@ -1,33 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    //THIS IS THE ONLY TIME we do direct assignment
-    //to this.state We always use setState
-    this.state = { lat: null, errorMesage: '' };
+    state = { lat: null, errorMessage: ''};
 
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        // We called setState!!!!
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message });
-      }
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
     );
   }
-  //React says that we have to define render!!
+
+    //React says that we have to define render!!
   render() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />
     }
-
+    
     return <div>Loading!</div>;
   }
 }
